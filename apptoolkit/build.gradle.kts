cf8c4187d013f9637e67ed59ca5edf483ebf78e6
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-01-13 08:26:56 +0200

    docs: Updated documentation with detailed descriptions
    - Added comprehensive documentation to various classes and enums, including NavigationDrawerItem, UiErrorModel, ErrorType, DebugAdsConstants, and ButtonState.
    - Improved clarity and understanding of code functionality.
    - Enhanced maintainability and readability of the project.
    - Updated library version to 0.0.15.

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -100,7 +100,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.D4rK7355608"
             artifactId = "AppToolkit"
-            version = "0.0.14"
+            version = "0.0.15"
 
             afterEvaluate {
                 from(components["release"])
--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/data/model/ui/animations/button/ButtonState.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/data/model/ui/animations/button/ButtonState.kt
@@ -1,3 +1,6 @@
 package com.d4rk.android.libs.apptoolkit.data.model.ui.animations.button
 
+/**
+ * @brief Represents the possible states of a button.
+ */
 enum class ButtonState { Pressed , Idle }
