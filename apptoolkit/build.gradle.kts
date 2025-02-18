9942b64f9260946c133660a42d17bafdac3ba451
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-02-18 21:38:35 +0200

    feat: Update SmallFloatingActionButton and bump library version
    - Updated `SmallFloatingActionButton` to accept a `Modifier` parameter for improved customization.
    - Bumped the library version to 0.0.59.

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -111,7 +111,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.D4rK7355608"
             artifactId = "AppToolkit"
-            version = "0.0.58"
+            version = "0.0.59"
 
             afterEvaluate {
                 from(components["release"])
