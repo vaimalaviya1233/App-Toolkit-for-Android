a9bd47d16a8e70015f4008382feaef81b1fe55ca
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-02-18 11:36:50 +0200

    chore: Bump version to 0.0.57

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -111,7 +111,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.D4rK7355608"
             artifactId = "AppToolkit"
-            version = "0.0.56"
+            version = "0.0.57"
 
             afterEvaluate {
                 from(components["release"])
