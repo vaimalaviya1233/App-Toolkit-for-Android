2e4ad60adb209ac277edb637eb11debbb484f6f0
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-02-25 20:43:21 +0200

    feat: Update dependencies and Gradle version, bump library version
    - Updated the library version to 0.0.69.
    - Updated the Gradle wrapper to version 8.13.
    - Updated the Coil dependency to version 3.1.0.
    - Updated the Play Services Ads dependency to version 24.0.0.

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -111,7 +111,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.D4rK7355608"
             artifactId = "AppToolkit"
-            version = "0.0.68"
+            version = "0.0.69"
 
             afterEvaluate {
                 from(components["release"])
