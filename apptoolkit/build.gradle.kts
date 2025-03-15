c694a7cc37004ebe1e674a06416f9128413605fc
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-03-15 21:37:30 +0200

    feat: Update AppToolkit library version and make AppInfoHelper methods public
    - Updated the library version to 0.0.78.
    - Changed `isAppInstalled` and `openApp` methods in `AppInfoHelper` to be public.

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -114,7 +114,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.D4rK7355608"
             artifactId = "AppToolkit"
-            version = "0.0.77"
+            version = "0.0.78"
 
             afterEvaluate {
                 from(components["release"])
--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/utils/helpers/AppInfoHelper.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/utils/helpers/AppInfoHelper.kt
@@ -14,7 +14,7 @@ object AppInfoHelper {
      * @param packageName The package name of the app to check.
      * @return True if the app is installed, false otherwise.
      */
-    private fun isAppInstalled(context: Context, packageName: String): Boolean {
+     fun isAppInstalled(context: Context, packageName: String): Boolean {
         return try {
             context.packageManager.getApplicationInfo(packageName, 0)
             true
@@ -29,7 +29,7 @@ object AppInfoHelper {
      * @param packageName The package name of the app to open.
      * @return True if the app was opened, false if not installed.
      */
-    private fun openApp(context: Context , packageName: String): Boolean {
+     fun openApp(context: Context , packageName: String): Boolean {
         val intent = context.packageManager.getLaunchIntentForPackage(packageName)
         val res: Resources = context.resources
         return if (intent != null) {
