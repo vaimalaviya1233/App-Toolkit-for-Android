a264922632fea836a4330274daaf5c5aaaf78b8d
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-03-15 21:58:11 +0200

    refactor: Improve app installation check and app opening logic
    - Refactored `AppInfoHelper` to an `open` class.
    - Replaced the `isAppInstalled` function logic with a more concise version that leverages `runCatching`.
    - Replaced the `openApp` function logic with a more concise version that uses `runCatching` and `let`, and simplified the toast message to directly use `context.getString()`.

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/utils/helpers/AppInfoHelper.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/utils/helpers/AppInfoHelper.kt
@@ -1,43 +1,28 @@
 package com.d4rk.android.libs.apptoolkit.core.utils.helpers
 
 import android.content.Context
-import android.content.res.Resources
-import android.content.pm.PackageManager
 import android.widget.Toast
 import com.d4rk.android.libs.apptoolkit.R
 
-object AppInfoHelper {
+open class AppInfoHelper {
 
     /**
      * Checks if a specific app is installed on the device.
-     * @param context The application context.
-     * @param packageName The package name of the app to check.
      * @return True if the app is installed, false otherwise.
      */
-     fun isAppInstalled(context: Context, packageName: String): Boolean {
-        return try {
-            context.packageManager.getApplicationInfo(packageName, 0)
-            true
-        } catch (e: PackageManager.NameNotFoundException) {
-            false
-        }
-    }
+    fun isAppInstalled(context : Context , packageName : String) : Boolean = runCatching { context.packageManager.getApplicationInfo(packageName , 0) }.isSuccess
 
     /**
      * Opens a specific app if installed.
-     * @param context The application context.
-     * @param packageName The package name of the app to open.
-     * @return True if the app was opened, false if not installed.
+     * @return True if the app was opened, false otherwise.
      */
-     fun openApp(context: Context , packageName: String): Boolean {
-        val intent = context.packageManager.getLaunchIntentForPackage(packageName)
-        val res: Resources = context.resources
-        return if (intent != null) {
-            context.startActivity(intent)
+    fun openApp(context : Context , packageName : String) : Boolean = runCatching {
+        context.packageManager.getLaunchIntentForPackage(packageName)?.let {
+            context.startActivity(it)
             true
-        } else {
-            Toast.makeText(context , res.getString(R.string.app_not_installed) , Toast.LENGTH_SHORT).show()
-            false
-        }
+        } ?: false
+    }.getOrElse {
+        Toast.makeText(context , context.getString(R.string.app_not_installed) , Toast.LENGTH_SHORT).show()
+        false
     }
 }
