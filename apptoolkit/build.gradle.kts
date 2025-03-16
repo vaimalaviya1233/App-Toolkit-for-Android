f4826c8a9bf48ad78aac51b0ba88a08014c00fed
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-03-16 02:12:00 +0200

    feat: Bump library version to 0.0.81
    - Updated the library version to 0.0.81.

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -114,7 +114,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.D4rK7355608"
             artifactId = "AppToolkit"
-            version = "0.0.80"
+            version = "0.0.81"
 
             afterEvaluate {
                 from(components["release"])
