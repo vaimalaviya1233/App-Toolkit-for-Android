3a66ab30ce9aa046d56d0c5f467d7ae5d02c9d16
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-03-16 11:31:46 +0200

    feat: Add SupportActivity, bump library version
    - Added `SupportActivity` to the manifest.
    - Updated the library version to 0.0.85.

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -114,7 +114,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.D4rK7355608"
             artifactId = "AppToolkit"
-            version = "0.0.84"
+            version = "0.0.85"
 
             afterEvaluate {
                 from(components["release"])
