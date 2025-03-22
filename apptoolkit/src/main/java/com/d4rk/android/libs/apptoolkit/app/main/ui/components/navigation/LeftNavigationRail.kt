28e1a5bc6157a823dd6eaf771a9722f143c1204b
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-03-22 08:37:01 +0200

    feat: Animate left navigation rail
    Adds animations to the left navigation rail, improving UI/UX.

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/main/ui/components/navigation/LeftNavigationRail.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/main/ui/components/navigation/LeftNavigationRail.kt
@@ -1,8 +1,16 @@
 package com.d4rk.android.libs.apptoolkit.app.main.ui.components.navigation
 
-import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -36,36 +44,36 @@ fun LeftNavigationRail(
     content : @Composable () -> Unit ,
     modifier : Modifier = Modifier
 ) {
-    val railWidth : Dp by animateDpAsState(
-        targetValue = if (isRailExpanded) 200.dp else 72.dp , animationSpec = tween(durationMillis = 300)
-    )
+    val railWidth : Dp by animateDpAsState(targetValue = if (isRailExpanded) 200.dp else 72.dp , animationSpec = tween(durationMillis = 300))
+    val textEntryAnimation : EnterTransition = fadeIn(animationSpec = tween(durationMillis = 300)) + expandHorizontally() + expandVertically()
+    val textExitAnimation : ExitTransition = fadeOut(animationSpec = tween(durationMillis = 300)) + shrinkHorizontally() + shrinkVertically()
 
     Row(modifier = modifier.padding(top = paddingValues.calculateTopPadding())) {
         NavigationRail(modifier = Modifier.width(railWidth)) {
-            bottomItems.forEach { item ->
-                NavigationRailItem(modifier = Modifier.bounceClick() , selected = currentRoute == item.route , onClick = { onBottomItemClick(item) } , icon = {
-                    Icon(imageVector = if (currentRoute == item.route) item.selectedIcon else item.icon , contentDescription = stringResource(id = item.title))
-                } , label = if (isRailExpanded) {
-                    { Text(text = stringResource(id = item.title) , maxLines = 1 , modifier = Modifier.animateContentSize() , overflow = TextOverflow.Ellipsis) }
-                }
-                else null)
+            bottomItems.forEach { item : BottomNavigationItem ->
+                val isSelected : Boolean = currentRoute == item.route
+
+                NavigationRailItem(modifier = Modifier.bounceClick() , selected = isSelected , onClick = { onBottomItemClick(item) } , icon = {
+                    Icon(imageVector = if (isSelected) item.selectedIcon else item.icon , contentDescription = stringResource(item.title) , modifier = Modifier.bounceClick())
+                } , label = {
+                    AnimatedVisibility(visible = isRailExpanded , enter = textEntryAnimation , exit = textExitAnimation) {
+                        Text(text = stringResource(id = item.title) , maxLines = 1 , overflow = TextOverflow.Ellipsis)
+                    }
+                })
             }
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            drawerItems.forEach { item ->
+            Spacer(modifier = Modifier.weight(weight = 1f))
+            drawerItems.forEach { item : NavigationDrawerItem ->
                 NavigationRailItem(selected = false , onClick = { onDrawerItemClick(item) } , icon = {
-                    Icon(
-                        imageVector = item.selectedIcon , contentDescription = stringResource(id = item.title)
-                    )
-                } , label = if (isRailExpanded) {
-                    { Text(text = stringResource(id = item.title)) }
-                }
-                else null)
+                    Icon(imageVector = item.selectedIcon , contentDescription = stringResource(id = item.title) , modifier = Modifier.bounceClick())
+                } , label = {
+                    AnimatedVisibility(visible = isRailExpanded , enter = textEntryAnimation , exit = textExitAnimation) {
+                        Text(text = stringResource(id = item.title))
+                    }
+                })
             }
         }
 
-        Box(modifier = Modifier.weight(1f)) {
+        Box(modifier = Modifier.weight(weight = 1f)) {
             content()
         }
     }
