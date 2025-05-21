413ce9fa3289008e254579a2599339cb2ca2bc86
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-05-21 16:07:35 +0300

    feat: Add LifecycleEffect and individual lifecycle event effects
    - Added `LifecycleEffect` composable to trigger actions based on specific lifecycle events.
    - Added individual composables for each lifecycle event:
        - `OnAnyEffect`
        - `OnCreateEffect`
        - `OnDestroyEffect`
        - `OnPauseEffect`
        - `OnStartEffect`
        - `OnStopEffect`
    - Refactored `OnResumeEffect` to use the new `LifecycleEffect`.

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/ui/effects/OnAnyEffect.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/ui/effects/OnAnyEffect.kt
@@ -4,6 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.Lifecycle
 
 @Composable
-fun OnResumeEffect(onResume: () -> Unit) {
-    LifecycleEffect(lifecycleEvent = Lifecycle.Event.ON_RESUME, onEvent = onResume)
+fun OnAnyEffect(onAny: () -> Unit) {
+    LifecycleEffect(lifecycleEvent = Lifecycle.Event.ON_ANY , onEvent = onAny)
 }
--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/ui/effects/OnPauseEffect.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/ui/effects/OnPauseEffect.kt
@@ -4,6 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.Lifecycle
 
 @Composable
-fun OnResumeEffect(onResume: () -> Unit) {
-    LifecycleEffect(lifecycleEvent = Lifecycle.Event.ON_RESUME, onEvent = onResume)
+fun OnPauseEffect(onPause: () -> Unit) {
+    LifecycleEffect(lifecycleEvent = Lifecycle.Event.ON_PAUSE , onEvent = onPause)
 }
