d3dcb92ea26d8fdd790e799a1b01ac65a6feb4a6
D4rK7355608 <d4rk7355608@gmail.com>
2025-06-02 11:07:35 +0300

    added support for new languages

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/utils/constants/links/AppLinks.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/utils/constants/links/AppLinks.kt
@@ -10,12 +10,12 @@ object AppLinks {
     const val MARKET_APP_PAGE : String = "market://details?id="
 
     // Legal & Policy
-    private const val GOOGLE_SITES_BASE : String = "https://sites.google.com/view/d4rk7355608/"
-    const val ADS_HELP_CENTER : String = "${GOOGLE_SITES_BASE}more/apps/ads-help-center"
-    const val PRIVACY_POLICY : String = "${GOOGLE_SITES_BASE}more/apps/privacy-policy"
-    const val TERMS_OF_SERVICE : String = "${GOOGLE_SITES_BASE}more/apps/terms-of-service"
-    const val CODE_OF_CONDUCT : String = "${GOOGLE_SITES_BASE}more/code-of-conduct"
-    const val LEGAL_NOTICES : String = "${GOOGLE_SITES_BASE}more/apps/legal-notices"
+    private const val GOOGLE_SITES_BASE : String = "https://d4rk7355608.github.io/profile/"
+    const val ADS_HELP_CENTER : String = "${GOOGLE_SITES_BASE}#ads-help-center"
+    const val PRIVACY_POLICY : String = "${GOOGLE_SITES_BASE}#privacy-policy-apps"
+    const val TERMS_OF_SERVICE : String = "${GOOGLE_SITES_BASE}#terms-of-service-apps"
+    const val CODE_OF_CONDUCT : String = "${GOOGLE_SITES_BASE}#code-of-conduct-website"
+    const val LEGAL_NOTICES : String = "${GOOGLE_SITES_BASE}#legal-notices"
     const val GPL_V3 : String = "https://www.gnu.org/licenses/gpl-3.0"
 
     // GitHub
