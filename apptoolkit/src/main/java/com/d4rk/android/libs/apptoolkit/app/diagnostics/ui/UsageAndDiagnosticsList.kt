03be177673c758bdcaac0aad02e698c0b8adf0e1
D4rK <d4rk7355608@gmail.com>
2025-06-04 08:59:00 +0300

    fix default consents

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/diagnostics/ui/UsageAndDiagnosticsList.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/diagnostics/ui/UsageAndDiagnosticsList.kt
@@ -52,14 +52,14 @@ fun UsageAndDiagnosticsList(paddingValues: PaddingValues, configProvider: BuildI
         dataStore.usageAndDiagnostics(default = !configProvider.isDebugBuild)
             .collectAsState(initial = !configProvider.isDebugBuild)
 
-    val analyticsState: State<Boolean> = dataStore.analyticsConsent(default = false)
-        .collectAsState(initial = configProvider.isDebugBuild)
-    val adStorageState: State<Boolean> = dataStore.adStorageConsent(default = false)
-        .collectAsState(initial = configProvider.isDebugBuild)
-    val adUserDataState: State<Boolean> = dataStore.adUserDataConsent(default = false)
-        .collectAsState(initial = configProvider.isDebugBuild)
-    val adPersonalizationState: State<Boolean> = dataStore.adPersonalizationConsent(default = false)
-        .collectAsState(initial = configProvider.isDebugBuild)
+    val analyticsState: State<Boolean> = dataStore.analyticsConsent(default = !configProvider.isDebugBuild)
+        .collectAsState(initial = !configProvider.isDebugBuild)
+    val adStorageState: State<Boolean> = dataStore.adStorageConsent(default = !configProvider.isDebugBuild)
+        .collectAsState(initial = !configProvider.isDebugBuild)
+    val adUserDataState: State<Boolean> = dataStore.adUserDataConsent(default = !configProvider.isDebugBuild)
+        .collectAsState(initial = !configProvider.isDebugBuild)
+    val adPersonalizationState: State<Boolean> = dataStore.adPersonalizationConsent(default = !configProvider.isDebugBuild)
+        .collectAsState(initial = !configProvider.isDebugBuild)
 
 
     var advancedSettingsExpanded: Boolean by remember { mutableStateOf<Boolean>(value = false) }
