d4bb07049d50c92cb1ae2cab5e455ba016a7dbf0
D4rK7355608 <d4rk7355608@gmail.com>
2025-06-10 11:10:26 +0300

    build: Increment versionCode to 27
    - Updated the `versionCode` in `app/build.gradle.kts` from 26 to 27.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.d4rk.android.apps.apptoolkit"
         minSdk = 23
         targetSdk = 36
-        versionCode = 26
+        versionCode = 27
         versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage") androidResources.localeFilters += listOf(
