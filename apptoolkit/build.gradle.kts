13a5dc61da37920700c013dc9f8de673efd824a5
D4rK7355608 <d4rk7355608@gmail.com>
2025-06-19 06:20:46 +0300

    feat: Update AppToolkit version to 1.0.31
    - Updated the AppToolkit library version from `1.0.30` to `1.0.31`.

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -122,7 +122,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.D4rK7355608"
             artifactId = "AppToolkit"
-            version = "1.0.30"
+            version = "1.0.31"
 
             afterEvaluate {
                 from(components["release"])
