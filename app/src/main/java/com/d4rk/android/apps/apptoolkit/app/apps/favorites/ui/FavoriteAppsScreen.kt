0ac77f8af6788ba7fb490482c72473eeb7c2c653
D4rK7355608 <d4rk7355608@gmail.com>
2025-06-24 22:45:25 +0300

    feat: Remove retry button from empty favorites screen
    The retry button on the "No apps added to favorites" screen was removed.
    The screen will now only display the message and an Android icon.

--- app/src/main/java/com/d4rk/android/apps/apptoolkit/app/apps/favorites/ui/FavoriteAppsScreen.kt
+++ app/src/main/java/com/d4rk/android/apps/apptoolkit/app/apps/favorites/ui/FavoriteAppsScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import com.d4rk.android.apps.apptoolkit.R
-import com.d4rk.android.apps.apptoolkit.app.apps.favorites.domain.actions.FavoriteAppsEvent
 import com.d4rk.android.apps.apptoolkit.app.apps.list.domain.model.ui.UiHomeScreen
 import com.d4rk.android.apps.apptoolkit.app.apps.list.ui.components.AppsList
 import com.d4rk.android.apps.apptoolkit.app.apps.list.ui.components.screens.loading.HomeLoadingScreen
@@ -24,9 +23,7 @@ fun FavoriteAppsScreen(paddingValues: PaddingValues) {
     ScreenStateHandler(
         screenState = screenState,
         onLoading = { HomeLoadingScreen(paddingValues = paddingValues) },
-        onEmpty = { NoDataScreen(textMessage = R.string.no_apps_added_to_favorites,icon = Icons.Outlined.Android ,showRetry = true, onRetry = {
-            viewModel.onEvent(FavoriteAppsEvent.LoadFavorites)
-        })},
+        onEmpty = { NoDataScreen(textMessage = R.string.no_apps_added_to_favorites,icon = Icons.Outlined.Android)},
 
         onSuccess = { uiHomeScreen ->
             val favorites by viewModel.favorites.collectAsState()
