72bca2423e704ca58c52217b56a6e2d61a5abfed
D4rK <d4rk7355608@gmail.com>
2025-07-02 14:34:01 +0300

    Fix SupportViewModel unit test flow

--- apptoolkit/src/test/java/com/d4rk/android/libs/apptoolkit/app/support/ui/TestSupportViewModel.kt
+++ apptoolkit/src/test/java/com/d4rk/android/libs/apptoolkit/app/support/ui/TestSupportViewModel.kt
@@ -1,14 +1,19 @@
 package com.d4rk.android.libs.apptoolkit.app.support.ui
 
 import com.android.billingclient.api.ProductDetails
+import com.d4rk.android.libs.apptoolkit.R
 import com.d4rk.android.libs.apptoolkit.app.support.domain.actions.SupportEvent
 import com.d4rk.android.libs.apptoolkit.core.MainDispatcherExtension
 import com.d4rk.android.libs.apptoolkit.core.domain.model.network.DataState
 import com.d4rk.android.libs.apptoolkit.core.domain.model.network.Errors
+import com.d4rk.android.libs.apptoolkit.core.domain.model.ui.ScreenState
+import com.d4rk.android.libs.apptoolkit.core.utils.helpers.UiTextHelper
+import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 
@@ -35,18 +40,46 @@ class TestSupportViewModel : TestSupportViewModelBase() {
             emit(DataState.Success<Map<String, ProductDetails>, Errors>(details))
         }
         setup(flow = flow, testDispatcher = dispatcherExtension.testDispatcher)
-        viewModel.onEvent(SupportEvent.QueryProductDetails(billingClient))
-        viewModel.uiState.testSuccess(expectedSize = details.size, testDispatcher = dispatcherExtension.testDispatcher)
+
+        viewModel.uiState.test {
+            viewModel.onEvent(SupportEvent.QueryProductDetails(billingClient))
+
+            val first = awaitItem()
+            assertTrue(first.screenState is ScreenState.IsLoading)
+            dispatcherExtension.testDispatcher.scheduler.advanceUntilIdle()
+
+            val second = awaitItem()
+            assertTrue(second.screenState is ScreenState.Success)
+            assertThat(second.data?.productDetails?.size).isEqualTo(details.size)
+            cancelAndIgnoreRemainingEvents()
+        }
     }
 
     @Test
     fun `query product details error`() = runTest(dispatcherExtension.testDispatcher) {
         val flow = flow {
             emit(DataState.Loading<Map<String, ProductDetails>, Errors>())
-            emit(DataState.Error<Map<String, ProductDetails>, Errors>(null, Errors.UseCase.FAILED_TO_LOAD_SKU_DETAILS))
+            emit(
+                DataState.Error<Map<String, ProductDetails>, Errors>(
+                    null,
+                    Errors.UseCase.FAILED_TO_LOAD_SKU_DETAILS
+                )
+            )
         }
         setup(flow = flow, testDispatcher = dispatcherExtension.testDispatcher)
-        viewModel.onEvent(SupportEvent.QueryProductDetails(billingClient))
-        viewModel.uiState.testError(testDispatcher = dispatcherExtension.testDispatcher)
+
+        viewModel.uiState.test {
+            viewModel.onEvent(SupportEvent.QueryProductDetails(billingClient))
+
+            val first = awaitItem()
+            assertTrue(first.screenState is ScreenState.IsLoading)
+            dispatcherExtension.testDispatcher.scheduler.advanceUntilIdle()
+
+            val second = awaitItem()
+            assertTrue(second.screenState is ScreenState.Error)
+            val msg = second.errors.first().message as UiTextHelper.StringResource
+            assertThat(msg.resourceId).isEqualTo(R.string.error_failed_to_load_sku_details)
+            cancelAndIgnoreRemainingEvents()
+        }
     }
 }
