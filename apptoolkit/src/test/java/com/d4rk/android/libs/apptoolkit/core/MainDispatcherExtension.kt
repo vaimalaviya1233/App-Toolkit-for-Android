ad8ac1bc9703932e43d15fed1356aac591b31d8a
D4rK7355608 <d4rk7355608@gmail.com>
2025-07-05 21:45:51 +0300

    Test: Use UnconfinedTestDispatcher for MainDispatcherExtension
    The `MainDispatcherExtension` now utilizes `UnconfinedTestDispatcher` instead of `StandardTestDispatcher`. This change aims to improve the execution of coroutines in tests by running them eagerly on the current thread.

--- apptoolkit/src/test/java/com/d4rk/android/libs/apptoolkit/core/MainDispatcherExtension.kt
+++ apptoolkit/src/test/java/com/d4rk/android/libs/apptoolkit/core/MainDispatcherExtension.kt
@@ -3,7 +3,7 @@ package com.d4rk.android.libs.apptoolkit.core
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.extension.AfterEachCallback
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.extension.ExtensionContext
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MainDispatcherExtension : BeforeEachCallback, AfterEachCallback {
-    val testDispatcher: TestDispatcher = StandardTestDispatcher()
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
 
     override fun beforeEach(context: ExtensionContext?) {
         Dispatchers.setMain(testDispatcher)
