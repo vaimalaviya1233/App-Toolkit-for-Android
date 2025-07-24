afbfc77d508132b476ab34690a0fd461749ba376
D4rK7355608 <d4rk7355608@gmail.com>
2025-07-24 21:55:28 +0300

    Update copyright and AppToolkit version
    - Changed copyright holder in `untranslatable_strings.xml` to "Mihai-Cristian Condrea".
    - Incremented AppToolkit library version to `1.0.43`.

--- apptoolkit/build.gradle.kts
+++ apptoolkit/build.gradle.kts
@@ -146,7 +146,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.MihaiCristianCondrea"
             artifactId = "App-Toolkit-for-Android"
-            version = "1.0.42"
+            version = "1.0.43"
 
             afterEvaluate {
                 from(components["release"])
