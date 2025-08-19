423acecf824748c9c7bd70bb6f443920f3b04427
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-08-19 11:34:39 +0300

    Refactor apps list to use repository layer

---
+++ app/src/main/java/com/d4rk/android/apps/apptoolkit/app/apps/list/data/repository/DeveloperAppsRepositoryImpl.kt
@@ -0,0 +1,45 @@
+package com.d4rk.android.apps.apptoolkit.app.apps.list.data.repository
+
+import com.d4rk.android.apps.apptoolkit.BuildConfig
+import com.d4rk.android.apps.apptoolkit.app.apps.list.data.model.api.ApiResponse
+import com.d4rk.android.apps.apptoolkit.app.apps.list.data.model.api.toAppInfo
+import com.d4rk.android.apps.apptoolkit.app.apps.list.domain.model.AppInfo
+import com.d4rk.android.apps.apptoolkit.app.apps.list.domain.repository.DeveloperAppsRepository
+import com.d4rk.android.apps.apptoolkit.app.apps.list.utils.constants.api.ApiConstants
+import com.d4rk.android.apps.apptoolkit.app.apps.list.utils.constants.api.ApiEnvironments
+import com.d4rk.android.apps.apptoolkit.app.apps.list.utils.constants.api.ApiPaths
+import com.d4rk.android.apps.apptoolkit.core.domain.model.network.Errors
+import com.d4rk.android.apps.apptoolkit.core.utils.extensions.toError
+import com.d4rk.android.libs.apptoolkit.core.domain.model.network.DataState
+import com.d4rk.android.libs.apptoolkit.core.domain.model.network.RootError
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class DeveloperAppsRepositoryImpl(
+    private val client: HttpClient
+) : DeveloperAppsRepository {
+
+    override fun fetchDeveloperApps(): Flow<DataState<List<AppInfo>, RootError>> = flow {
+        runCatching {
+            val url = BuildConfig.DEBUG.let { isDebug ->
+                val environment = if (isDebug) ApiEnvironments.ENV_DEBUG else ApiEnvironments.ENV_RELEASE
+                "${ApiConstants.BASE_REPOSITORY_URL}/$environment${ApiPaths.DEVELOPER_APPS_API}"
+            }
+            val httpResponse: HttpResponse = client.get(url)
+            val apiResponse: ApiResponse = httpResponse.body()
+
+            apiResponse.data.apps
+                .map { it.toAppInfo() }
+                .sortedBy { it.name.lowercase() }
+        }.onSuccess { apps ->
+            emit(DataState.Success(data = apps))
+        }.onFailure { error ->
+            emit(DataState.Error(error = error.toError(default = Errors.UseCase.FAILED_TO_LOAD_APPS)))
+        }
+    }
+}
+
