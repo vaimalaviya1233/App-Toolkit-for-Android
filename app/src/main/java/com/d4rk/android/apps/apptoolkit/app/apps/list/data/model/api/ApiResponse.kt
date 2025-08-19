0e00427207e309efb15397a3389513e7a9e53bdb
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-08-19 11:58:38 +0300

    Add AppInfoDto and mapping

--- app/src/main/java/com/d4rk/android/apps/apptoolkit/app/apps/list/data/model/api/ApiResponse.kt
+++ app/src/main/java/com/d4rk/android/apps/apptoolkit/app/apps/list/data/model/api/ApiResponse.kt
@@ -1,6 +1,5 @@
 package com.d4rk.android.apps.apptoolkit.app.apps.list.data.model.api
 
-import com.d4rk.android.apps.apptoolkit.app.apps.list.domain.model.AppInfo
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -11,19 +10,7 @@ data class ApiResponse(
 
 @Serializable
 data class AppDataWrapper(
-    @SerialName("apps") val apps: List<ApiAppInfo>
+    @SerialName("apps") val apps: List<AppInfoDto>
 )
 
-@Serializable
-data class ApiAppInfo(
-    @SerialName("name") val name: String,
-    @SerialName("packageName") val packageName: String,
-    @SerialName("iconLogo") val iconUrl: String,
-)
-
-fun ApiAppInfo.toAppInfo(): AppInfo = AppInfo(
-    name = name,
-    packageName = packageName,
-    iconUrl = iconUrl
-)
 
---
+++ app/src/main/java/com/d4rk/android/apps/apptoolkit/app/apps/list/data/model/api/AppInfoDto.kt
@@ -0,0 +1,25 @@
+package com.d4rk.android.apps.apptoolkit.app.apps.list.data.model.api
+
+import com.d4rk.android.apps.apptoolkit.app.apps.list.domain.model.AppInfo
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AppInfoDto(
+    @SerialName("name") val name: String,
+    @SerialName("packageName") val packageName: String,
+    @SerialName("iconLogo") val iconUrl: String,
+)
+
+fun AppInfoDto.toDomain(): AppInfo = AppInfo(
+    name = name,
+    packageName = packageName,
+    iconUrl = iconUrl,
+)
+
+fun AppInfo.toDto(): AppInfoDto = AppInfoDto(
+    name = name,
+    packageName = packageName,
+    iconUrl = iconUrl,
+)
+
