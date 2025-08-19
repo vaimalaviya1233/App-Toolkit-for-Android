03bc2e457aa6115483e7bc79ecc3d670129b0677
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-08-19 10:02:46 +0300

    Make app helper suspend and use IO dispatcher

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/utils/helpers/AppInfoHelper.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/utils/helpers/AppInfoHelper.kt
@@ -3,6 +3,8 @@ package com.d4rk.android.libs.apptoolkit.core.utils.helpers
 import android.content.Context
 import android.widget.Toast
 import com.d4rk.android.libs.apptoolkit.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 open class AppInfoHelper {
 
@@ -10,19 +12,25 @@ open class AppInfoHelper {
      * Checks if a specific app is installed on the device.
      * @return True if the app is installed, false otherwise.
      */
-    fun isAppInstalled(context : Context , packageName : String) : Boolean = runCatching { context.packageManager.getApplicationInfo(packageName , 0) }.isSuccess
+    suspend fun isAppInstalled(context : Context , packageName : String) : Boolean = withContext(Dispatchers.IO) {
+        runCatching { context.packageManager.getApplicationInfo(packageName , 0) }.isSuccess
+    }
 
     /**
      * Opens a specific app if installed.
      * @return True if the app was opened, false otherwise.
      */
-    fun openApp(context : Context , packageName : String) : Boolean = runCatching {
-        context.packageManager.getLaunchIntentForPackage(packageName)?.let {
-            context.startActivity(it)
+    suspend fun openApp(context : Context , packageName : String) : Boolean {
+        val launchIntent = withContext(Dispatchers.IO) {
+            runCatching { context.packageManager.getLaunchIntentForPackage(packageName) }.getOrNull()
+        }
+
+        return if (launchIntent != null) {
+            context.startActivity(launchIntent)
             true
-        } == true
-    }.getOrElse {
-        Toast.makeText(context , context.getString(R.string.app_not_installed) , Toast.LENGTH_SHORT).show()
-        false
+        } else {
+            Toast.makeText(context , context.getString(R.string.app_not_installed) , Toast.LENGTH_SHORT).show()
+            false
+        }
     }
 }
