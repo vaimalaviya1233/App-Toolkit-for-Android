1a6b6fbf5d52e427a285f0063d3690b07fc68112
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-08-25 10:32:00 +0300

    Make favorites repository context-agnostic

--- app/src/main/java/com/d4rk/android/apps/apptoolkit/app/apps/favorites/domain/usecases/ToggleFavoriteUseCase.kt
+++ app/src/main/java/com/d4rk/android/apps/apptoolkit/app/apps/favorites/domain/usecases/ToggleFavoriteUseCase.kt
@@ -2,10 +2,18 @@ package com.d4rk.android.apps.apptoolkit.app.apps.favorites.domain.usecases
 
 import com.d4rk.android.apps.apptoolkit.app.apps.favorites.domain.repository.FavoritesRepository
 import com.d4rk.android.libs.apptoolkit.core.domain.usecases.Repository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class ToggleFavoriteUseCase(private val repository: FavoritesRepository) : Repository<String, Unit> {
+class ToggleFavoriteUseCase(
+    private val repository: FavoritesRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : Repository<String, Unit> {
     override suspend operator fun invoke(param: String) {
-        repository.toggleFavorite(param)
+        withContext(dispatcher) {
+            repository.toggleFavorite(param)
+        }
     }
 }
 
--- app/src/main/java/com/d4rk/android/apps/apptoolkit/core/data/favorites/FavoritesRepositoryImpl.kt
+++ app/src/main/java/com/d4rk/android/apps/apptoolkit/core/data/favorites/FavoritesRepositoryImpl.kt
@@ -2,17 +2,13 @@ package com.d4rk.android.apps.apptoolkit.core.data.favorites
 
 import com.d4rk.android.apps.apptoolkit.app.apps.favorites.domain.repository.FavoritesRepository
 import com.d4rk.android.apps.apptoolkit.core.data.datastore.DataStore
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
 
 class FavoritesRepositoryImpl(private val dataStore: DataStore) : FavoritesRepository {
     override fun observeFavorites(): Flow<Set<String>> = dataStore.favoriteApps
 
     override suspend fun toggleFavorite(packageName: String) {
-        withContext(context = Dispatchers.IO) {
-            dataStore.toggleFavoriteApp(packageName)
-        }
+        dataStore.toggleFavoriteApp(packageName)
     }
 }
 
