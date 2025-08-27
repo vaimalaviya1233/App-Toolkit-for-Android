421f5d97ca49c7d4beb8c8a05e79a66b8dc4c577
D4rK7355608 <d4rk7355608@gmail.com>
2025-08-27 23:30:02 +0300

    Refactor OnboardingActivity to simplify UI
    Removed unnecessary Surface composable in `OnboardingActivity` as `AppTheme` already provides a background.

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/onboarding/ui/OnboardingActivity.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/onboarding/ui/OnboardingActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import com.d4rk.android.libs.apptoolkit.app.theme.style.AppTheme
@@ -29,12 +25,7 @@ class OnboardingActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    OnboardingScreen()
-                }
+                OnboardingScreen()
             }
         }
     }
