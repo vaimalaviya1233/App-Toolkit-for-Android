b08354cff2420f40f5efe0e3b0c046f18143d25d
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-08-27 23:56:57 +0300

    Introduce PermissionsRepository contract

---
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/permissions/data/PermissionsRepository.kt
@@ -0,0 +1,18 @@
+package com.d4rk.android.libs.apptoolkit.app.permissions.data
+
+import com.d4rk.android.libs.apptoolkit.app.settings.settings.domain.model.SettingsConfig
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository that exposes the permissions configuration.
+ *
+ * Implementations should be free of Android framework dependencies so that
+ * the UI layer can obtain the configuration without requiring a [Context].
+ */
+interface PermissionsRepository {
+    /**
+     * Returns a stream of the permissions configuration to be displayed by the UI.
+     */
+    fun getPermissionsConfig(): Flow<SettingsConfig>
+}
+
--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/permissions/domain/actions/PermissionsAction.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/permissions/domain/actions/PermissionsAction.kt
@@ -2,4 +2,12 @@ package com.d4rk.android.libs.apptoolkit.app.permissions.domain.actions
 
 import com.d4rk.android.libs.apptoolkit.core.ui.base.handling.ActionEvent
 
+/**
+ * Side-effect actions sent from [com.d4rk.android.libs.apptoolkit.app.permissions.ui.PermissionsViewModel]
+ * to the UI layer.
+ *
+ * Currently no actions are defined but this sealed interface leaves room for
+ * future one-off events such as navigation.
+ */
 sealed interface PermissionsAction : ActionEvent
+
