8037598d811bfa718621d1c00881f334870ec934
D4rK7355608 <d4rk7355608@gmail.com>
2025-08-27 23:15:17 +0300

    Refactor LicensesActivity to simplify UI
    Removed unnecessary Surface composable in `LicensesActivity` as `AppTheme` already provides a background.

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/licenses/LicensesActivity.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/licenses/LicensesActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import com.d4rk.android.libs.apptoolkit.app.theme.style.AppTheme
 
 class LicensesActivity : AppCompatActivity() {
@@ -16,12 +12,7 @@ class LicensesActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Surface(
-                    modifier = Modifier.Companion.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    LicensesScreen(activity = this@LicensesActivity)
-                }
+                LicensesScreen(activity = this@LicensesActivity)
             }
         }
     }
