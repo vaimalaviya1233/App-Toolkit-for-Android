30a48951739d6f1c62a9ea0b972c12b5eeded87b
D4rK7355608 <d4rk7355608@gmail.com>
2025-08-28 08:27:06 +0300

    Refactor AboutViewModel for Kotlin best practices
    Refactor `AboutViewModel` to use `runCatching` for error handling, improving code conciseness and adhering to Kotlin's best practices for exception management.

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/about/ui/AboutViewModel.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/about/ui/AboutViewModel.kt
@@ -10,6 +10,7 @@ import com.d4rk.android.libs.apptoolkit.core.domain.model.ui.UiSnackbar
 import com.d4rk.android.libs.apptoolkit.core.domain.model.ui.UiStateScreen
 import com.d4rk.android.libs.apptoolkit.core.domain.model.ui.dismissSnackbar
 import com.d4rk.android.libs.apptoolkit.core.domain.model.ui.showSnackbar
+import com.d4rk.android.libs.apptoolkit.core.domain.model.ui.successData
 import com.d4rk.android.libs.apptoolkit.core.ui.base.ScreenViewModel
 import com.d4rk.android.libs.apptoolkit.core.utils.constants.ui.ScreenMessageType
 import com.d4rk.android.libs.apptoolkit.core.utils.helpers.UiTextHelper
@@ -33,19 +34,19 @@ open class AboutViewModel(
 
     private fun loadAboutInfo() {
         viewModelScope.launch {
-            try {
+            runCatching {
                 val info = repository.getAboutInfo()
                 screenState.successData { info }
-            } catch (e: Exception) {
-                screenState.showSnackbar(
-                    snackbar = UiSnackbar(
-                        message = UiTextHelper.StringResource(resourceId = R.string.snack_device_info_failed),
-                        isError = true,
-                        timeStamp = System.nanoTime(),
-                        type = ScreenMessageType.SNACKBAR,
-                    ),
-                )
-            }
+            }.onFailure {
+                    screenState.showSnackbar(
+                        snackbar = UiSnackbar(
+                            message = UiTextHelper.StringResource(resourceId = R.string.snack_device_info_failed),
+                            isError = true,
+                            timeStamp = System.nanoTime(),
+                            type = ScreenMessageType.SNACKBAR,
+                        ),
+                    )
+                }
         }
     }
 
