3b85fabac87f088492060ebb9960da6e7bf19572
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-08-30 03:34:28 +0300

    Use coroutine to process past purchases

--- app/src/main/java/com/d4rk/android/apps/apptoolkit/AppToolkit.kt
+++ app/src/main/java/com/d4rk/android/apps/apptoolkit/AppToolkit.kt
@@ -14,6 +14,7 @@ import com.d4rk.android.libs.apptoolkit.data.core.BaseCoreManager
 import com.d4rk.android.libs.apptoolkit.data.core.ads.AdsCoreManager
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
 import org.koin.android.ext.android.getKoin
 
@@ -41,7 +42,9 @@ class AppToolkit : BaseCoreManager(), DefaultLifecycleObserver {
     }
 
     override fun onResume(owner: LifecycleOwner) {
-        billingRepository.processPastPurchases()
+        owner.lifecycleScope.launch {
+            billingRepository.processPastPurchases()
+        }
     }
 
     override fun onActivityCreated(activity : Activity , savedInstanceState : Bundle?) {}
