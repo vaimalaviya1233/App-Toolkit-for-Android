869e8d831a47fbef46738487034c0e9982f9ee10
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2025-08-30 03:00:42 +0300

    Increment app version code
    - Incremented the app's version code to 56 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.d4rk.android.apps.apptoolkit"
         minSdk = 23
         targetSdk = 36
-        versionCode = 55
+        versionCode = 56
         versionName = "1.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage")
