2cadcd1ae6be1f9ccc126227302eeaba9242ffd6
D4rK7355608 <d4rk7355608@gmail.com>
2025-08-30 03:37:22 +0300

    Remove unused purchase initiation code from SupportActivity
    The `initiatePurchase` function and related `ProductDetails` import were removed from `SupportActivity` as they were unused.

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/support/ui/SupportActivity.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/app/support/ui/SupportActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.android.billingclient.api.ProductDetails
-import com.d4rk.android.libs.apptoolkit.app.support.billing.BillingRepository
 import com.d4rk.android.libs.apptoolkit.app.theme.style.AppTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -19,12 +17,6 @@ class SupportActivity : AppCompatActivity() {
             AppTheme {
                 SupportComposable(viewModel = viewModel , activity = this@SupportActivity)
             }
-        }
-    }
-
-    fun initiatePurchase(productId : String , productDetailsMap : Map<String , ProductDetails>) { // FIXME: Function "initiatePurchase" is never used
-        productDetailsMap[productId]?.let { productDetails : ProductDetails ->
-            BillingRepository.getInstance(this).launchPurchaseFlow(this , productDetails)
         }
     }
 }
