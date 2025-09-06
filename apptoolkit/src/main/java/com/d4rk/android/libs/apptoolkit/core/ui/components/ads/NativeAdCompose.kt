02e2beb239dd066e7e9fa4aec0bc920b64f90afc
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-09-06 11:23:35 +0300

    Bind NativeAdView using AndroidView update

--- apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/ui/components/ads/NativeAdCompose.kt
+++ apptoolkit/src/main/java/com/d4rk/android/libs/apptoolkit/core/ui/components/ads/NativeAdCompose.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
@@ -65,10 +64,9 @@ fun NativeAdView(
                 )
             }
         },
+        update = { it.setNativeAd(nativeAd) },
         modifier = modifier,
     )
-
-    LaunchedEffect(nativeAd) { nativeAdView.setNativeAd(nativeAd) }
 }
 
 /**
