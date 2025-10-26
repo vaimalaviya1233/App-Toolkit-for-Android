a44dcf0cd444d475a895dce7204839a7f22e831c
D4rK7355608 <d4rk7355608@gmail.com>
2025-10-26 22:26:16 +0200

    chore: Bump dependencies and version code
    - Upgrade Kotlin from version 2.2.20 to 2.2.21.
    - Increment the app version code from 78 to 79.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationIdSuffix = ".apptoolkit"
         minSdk = 23
         targetSdk = 36
-        versionCode = 78
+        versionCode = 79
         versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage")
